0c4f573b4e7f84db16d5698cea0b254d6cc4722a
Scott LeFoll <51521131+scottlefoll@users.noreply.github.com>
2023-07-19 17:30:42 -0400

    update

--- soilRegenApp/urls.py
+++ soilRegenApp/urls.py
@@ -7,8 +7,6 @@ from . import views
 
 
 urlpatterns = [
-    path("admin/", admin.site.urls),
-    path("accounts/", include("django.contrib.auth.urls")),
     path('', TemplateView.as_view(template_name="home.html"), name="home"),
     path("signup/", SignUpView.as_view(), name="signup"),
 
