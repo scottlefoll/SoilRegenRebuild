e738e4fbf74e529f7389dd5cbd75bb2732554146
Scott LeFoll <51521131+scottlefoll@users.noreply.github.com>
2023-07-20 09:52:42 -0400

    Update - base.html

--- soilRegenApp/static/soilRegenApp/js/scripts.js
+++ soilRegenApp/static/soilRegenApp/js/scripts.js
@@ -1,8 +1,49 @@
-const num1 = 5;
-const num2 = 3;
+// api url
+const api_url = "https://aravinds1811-neural-style-transfer.hf.space/+/api/predict/";
+  
+// Defining async function
+async function getapi(url) {
+    const params = "";
+    // Storing response
+    const response = await fetch(url + ", " + params);
+    
+    // Storing data in form of JSON
+    var data = await response.json();
+    console.log(data);
+    if (response) {
+        hideloader();
+    }
+    show(data);
+}
+// Calling that async function
+// getapi(api_url);
+  
+// Function to hide the loader
+function hideloader() {
+    document.getElementById('loading').style.display = 'none';
+    document.getElementById('loading').innerHTML = "Success!!";
+    alert("check console for data")
+}
+// Function to define innerHTML for HTML table
+function show(data) {
+    let tab = "Test";
+    
+    // Setting innerHTML as tab variable
+    document.getElementById("employees").innerHTML = tab;
+}
 
-// add two numbers
-const sum = num1 + num2;
+function setActiveLink() {
+    // Get all links with class='nav-choice'
+    var navLinks = document.getElementsByClassName('nav-choice');
 
-// display the sum
-console.log('The sum of ' + num1 + ' and ' + num2 + ' is: ' + sum);
+    // Loop through all navigation links
+    for (var i = 0; i < navLinks.length; i++) {
+        // If the navigation link matches the current URL, add the 'active' class
+        if (navLinks[i].href === document.URL) {
+            navLinks[i].className += ' active';
+        }
+    }
+}
+
+// Call the function when the page loads
+window.onload = setActiveLink;
